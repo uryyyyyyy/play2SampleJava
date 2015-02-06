da5f481c4cbf274f8d8f6440c72b59dd6277bfa0
shibata_ko <shibata_ko@worksap.co.jp>
2015-02-06 20:35:53 +0900

    evol

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,28 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table model1entity (
+  id                        bigint not null,
+  name                      varchar(255),
+  done                      boolean,
+  due_date                  timestamp,
+  constraint pk_model1entity primary key (id))
+;
+
+create sequence model1entity_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists model1entity;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists model1entity_seq;
+
